e70777321a81c0886a06f7fe781563440f476c43
miguelramirezml93 <jose.ramirez@mercadolibre.com.co>
2023-07-07 16:12:42 -0500

    Temporal options load change

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local pvim = require "api.pvim"
 local user_configs = require "user.configs"
 local user_langservs = require "user.ls"
 
-
+pvim.set_options(user_configs.options)
 pvim.setup({
     lsp = {
         lang_servers = user_langservs,
@@ -11,6 +11,5 @@ pvim.setup({
         specs = user_configs.plugin_specs,
     },
 })
-pvim.set_options(user_configs.options)
 pvim.set_colorscheme(user_configs.colorscheme)
 user_configs.setup_keymap(pvim)
